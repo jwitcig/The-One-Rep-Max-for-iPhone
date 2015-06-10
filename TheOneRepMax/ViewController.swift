8b9c1d250bad904bda46b037320adbf7a9c0f79a
jwitcig <jwitcig@gmail.com>
2015-06-09 23:52:26 -0500

    Initial Commit

---
+++ TheOneRepMax/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TheOneRepMax
+//
+//  Created by Application Development on 6/9/15.
+//  Copyright (c) 2015 JwitApps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
